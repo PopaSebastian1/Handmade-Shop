970daa6e19205621e97aacfe04e63a217ade3752
Bajan Ramona-Maria <102227689+kiwy37@users.noreply.github.com>
2025-06-06 18:31:43 +0300

    Update environment.ts

--- Frontend/src/app/services/google-auth/environment.ts
+++ Frontend/src/app/services/google-auth/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/Handmade-Shopping-1.0-SNAPSHOT/api'
+    apiUrl: 'http://54.166.166.136:8080/Handmade-Shopping-1.0-SNAPSHOT/api/'
   };
