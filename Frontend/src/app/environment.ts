29e5dafa3420f7f61192a58d5ebe80589be04146
Bajan Ramona-Maria <102227689+kiwy37@users.noreply.github.com>
2025-06-15 18:01:42 +0300

    Update environment.ts

--- Frontend/src/app/environment.ts
+++ Frontend/src/app/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:8181/Handmade-Shopping-1.0-SNAPSHOT/api'
+    apiUrl: 'https://54.166.166.136:8181/Handmade-Shopping-1.0-SNAPSHOT/api'
   };
